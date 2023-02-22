001adbfbb488baaafcbb9539099b5c37aa52f0ab
Nari Park <nari36@naver.com>
2023-02-22 16:30:07 -0500

    setup & added modal page & jqery cdn

---
+++ students.php
@@ -0,0 +1,98 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <title>Hello, world!</title>
+  </head>
+
+  <body>
+    <!-- Modal -->
+    <div class="modal fade" id="studentAddModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+      <div class="modal-dialog">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title" id="exampleModalLabel">Add Student</h5>
+            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+          </div>
+          <form id="saveStudent">
+            <div class="modal-body">
+              <div class="mb-3">
+                <label for="">Name</label>
+                <input type="text" name="name" class="form-control">
+              </div>
+              <div class="mb-3">
+                <label for="">Email</label>
+                <input type="text" name="email" class="form-control">
+              </div>
+              <div class="mb-3">
+                <label for="">Phone</label>
+                <input type="text" name="phone" class="form-control">
+              </div>
+              <div class="mb-3">
+                <label for="">Course</label>
+                <input type="text" name="course" class="form-control">
+              </div>
+            </div>
+            <div class="modal-footer">
+              <button type="button" class="btn btn-secondary"       data-bs-dismiss="modal">Close</button>
+              <button type="submit" class="btn btn-primary">Save Student</button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12">
+          <div class="card">
+            <div class="card-header">
+              <h4>PHP Ajax CRUD without page reload using Bootstrap Modal
+                <!-- Button trigger modal -->
+                <button 
+                  type="button" 
+                  class="btn btn-primary float-end" 
+                  data-bs-toggle="modal" 
+                  data-bs-target="#studentAddModal">
+                    Add Student
+                </button>
+              </h4>
+            </div>
+            <div class="card-body">
+
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+    <script>
+      $(document).on('submit', '#saveStudent', function (e) {
+        e.preventDefault();
+
+        var formData = new FormData(this);
+        formData.append("save_student", true);
+
+        $.ajax({
+          type: "POST",
+          url: "code.php",
+          data: formData,
+          processData: false,
+          contentType: false,
+          success: function (respond) {
+
+          }
+        });
+
+      });
+    </script>
+
+  </body>
+</html>
